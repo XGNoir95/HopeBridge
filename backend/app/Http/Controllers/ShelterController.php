37825ec6ad33e105ac7cec6bee6601730fb35b8f
Adnan-Shahriar-1190 <sb.adnan2020@gmail.com>
2025-02-14 04:58:51 +0600

    Added Shelter info and distance

--- backend/app/Http/Controllers/ShelterController.php
+++ backend/app/Http/Controllers/ShelterController.php
@@ -11,9 +11,52 @@ class ShelterController extends Controller
         $shelters = Shelter::all();
         return response()->json($shelters);
     }
+   //Single Shelter info
+   public function show($id)
+   {
+       $shelter = Shelter::findOrFail($id);
+   
+       $userLatitude = request()->input('latitude');
+       $userLongitude = request()->input('longitude');
+   
+       if (!$userLatitude || !$userLongitude) {
+           return response()->json(['error' => 'User location is required'], 400);
+       }
+   
+      //Distance from User to selected Shelter
+       $distance = $this->calculateDistance(
+           $userLatitude,
+           $userLongitude,
+           $shelter->latitude,
+           $shelter->longitude
+       );
+       return response()->json([
+           'shelter' => $shelter,
+           'user_location' => [
+               'latitude' => $userLatitude,
+               'longitude' => $userLongitude,
+           ],
+           'distance' => $distance,
+       ]);
+   }
+  //calculate distance between User and Shelter
+   private function calculateDistance($lat1, $lon1, $lat2, $lon2)
+   {
+       //In kilometers
+       $earthRadius = 6371; 
 
-    public function show(){
-        
-    }
+       $latFrom = deg2rad($lat1);
+       $lonFrom = deg2rad($lon1);
+       $latTo = deg2rad($lat2);
+       $lonTo = deg2rad($lon2);
+
+       $latDelta = $latTo - $latFrom;
+       $lonDelta = $lonTo - $lonFrom;
+
+       $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+           cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+
+       return round($angle * $earthRadius, 2);
+   }
    
 }
