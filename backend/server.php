192712fa5536ae6772909932a27b5a06d0ca7896
Adnan-Shahriar-1190 <sb.adnan2020@gmail.com>
2025-03-15 11:48:17 +0600

    server.php updated

--- backend/server.php
+++ backend/server.php
@@ -7,15 +7,17 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+$port = getenv('PORT') ?: 8080;
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
+// application without installing a real web server.
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
-require_once __DIR__.'/public/index.php';
+echo "Starting Laravel development server on port $port...\n";
+exec("php -S 0.0.0.0:$port -t public");
